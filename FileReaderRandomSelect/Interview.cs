a772191c5241a38a6d4cc44192a9015e940f790c
Menzi Khuzwayo <43129768+mpkayy16@users.noreply.github.com>
2024-04-07 18:38:38 +0200

    Add files via upload

---
+++ FileReaderRandomSelect/Interview.cs
@@ -0,0 +1,96 @@
+﻿namespace FileReaderRandomSelect
+{
+    public static class Interview
+    {
+        internal static void PracticeMode(List<string> questions, List<string> answers)
+        {
+            Random random = new Random();
+            while (questions.Count > 0)
+            {
+                int index = random.Next(questions.Count);
+                string randomQuestion = questions[index];
+                questions.RemoveAt(index);
+                var answersArray = answers[index].Split("~~");
+                answers.RemoveAt(index);
+
+                Console.WriteLine(randomQuestion);
+                var key = Console.ReadKey().Key;
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    answersArray?.ToList().ForEach(answer => Console.WriteLine($"==> {answer}"));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+        }
+
+        internal static void InterviewMode(List<string> questions, List<string> answers)
+        {
+            bool isInterviewComplete = false;
+            while (!isInterviewComplete)
+            {
+                Console.Write("Enter a search string:  ");
+                string searchString = Console.ReadLine().ToLower();
+                Console.WriteLine();
+                isInterviewComplete = searchString == ";";
+                Console.Clear();
+                if (isInterviewComplete)
+                {
+                    Console.WriteLine("Interview complete...");
+                    continue;
+                }
+                var questionsMatched = questions.Where(a => a.ToLower().Contains(searchString));
+                if (questionsMatched.Any())
+                {
+                    Console.Clear();
+                    foreach (var q in questionsMatched)
+                    {
+                        var indexOfQuestionMatched = questions.IndexOf(q);
+                        PrintQuestionAndAnswer(questions[indexOfQuestionMatched], answers[indexOfQuestionMatched]);
+                    }
+                }
+                else
+                {
+                    SearchByMatchCount(questions, answers, searchString);
+                }
+            }
+        }
+        static void SearchByMatchCount(List<string> questions, List<string> answers, string searchString)
+        {
+            string[] searchTerms = searchString.Split(' ');
+
+            Dictionary<int, int> matchCount = new Dictionary<int, int>();
+            for (int i = 0; i < questions.Count; i++)
+            {
+                int count = searchTerms.Count(term => questions[i].Contains(term));
+                if (count > 0)
+                {
+                    matchCount.Add(i, count);
+                }
+            }
+            var top3matches = matchCount.OrderByDescending(pair => pair.Value).Take(3);
+            foreach (var item in top3matches)
+            {
+                PrintQuestionAndAnswer(questions[item.Key], answers[item.Key]);
+            }
+        }
+
+        static void PrintQuestionAndAnswer(string question, string answer)
+        {
+            Console.WriteLine(question);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("==> " + answer);
+            Console.ForegroundColor = ConsoleColor.White;
+            //Console.WriteLine("~~");
+        }
+
+        public static void AskInterviewrsQuestions(List<string> myQuestions)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            myQuestions.ForEach(x => Console.WriteLine(x + "\n"));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ FileReaderRandomSelect/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FileReaderRandomSelect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var resources = GetResources();
+
+            Console.Write("What are you here for? Search or Practice (s/p): ");
+            string task = Console.ReadLine().ToLower();
+            bool isHereToPractice = task.StartsWith("p");
+            Console.Clear();
+            Console.WriteLine();
+
+            if (isHereToPractice)
+            {
+                Interview.PracticeMode(resources.questions, resources.answers);
+            }
+            else
+            {
+                Interview.InterviewMode(resources.questions, resources.answers);
+                Interview.AskInterviewrsQuestions(resources.questionsForInterviewrs);
+            }
+            Console.ReadKey();
+        }
+
+        private static dynamic GetResources()
+        {
+            List<string> questions = File.ReadAllLines("C:\\Users\\menzi.khuzwayo\\source\\repos\\FileReaderRandomSelect\\FileReaderRandomSelect\\Questions.txt").ToList();
+            List<string> answers = File.ReadAllLines("C:\\Users\\menzi.khuzwayo\\source\\repos\\FileReaderRandomSelect\\FileReaderRandomSelect\\answers.txt").ToList();
+            List<string> questionsForInterviewrs = File.ReadAllLines("C:\\Users\\menzi.khuzwayo\\source\\repos\\FileReaderRandomSelect\\FileReaderRandomSelect\\myQuestions.txt").ToList();
+
+            return new { questions , answers, questionsForInterviewrs };
+        }
+    }
+}
